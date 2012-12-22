78cebe29d1d69dbce982634e953a50d39609f865
siciarek <siciarek@gmail.com>
2012-12-22 23:34:27 +0100

    more configuration

---
+++ Controller/LocaleController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Siciarek\PhotoGalleryBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\Query;
+
+
+class LocaleController extends Controller
+{
+
+    /**
+     * @Route("/lang-{locale}.html", name="_change_locale", defaults={"locale"="pl"}, requirements = {"locale"="^[a-z]{2}$"})
+     * @Template()
+     */
+    public function setLocaleAction($locale) {
+
+        $locale = in_array($locale, array("en", "pl")) ? $locale : "en";
+
+        $session = $this->getRequest()->getSession();
+        $session->set("locale", $locale);
+
+        $referer = $this->getRequest()->server->get('HTTP_REFERER');
+        $referer = $referer == null ? $this->generateUrl("_photogallery_home") : $referer;
+
+        return $this->redirect($referer);
+    }
+}
