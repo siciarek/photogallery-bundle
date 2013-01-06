1ffec7c24e4248a1363dcbe0752b66ed2d1156ed
Jacek Siciarek <siciarek@gmail.com>
2013-01-06 01:07:48 +0100

    Update Composer/ScriptHandler.php

--- Composer/ScriptHandler.php
+++ Composer/ScriptHandler.php
@@ -12,37 +12,13 @@ class ScriptHandler
 {
     public static function installSubmodules($event)
     {
-    	$repodir = __DIR__ . "/.."; 
-        $gitdir = $repodir . "/.git";
-        
-		$cmd = array();
-        $cmd[] = "git status";
-        $cmd[] = "git submodule update --init";
-
-		echo "START INSTALLATION\n";
-		chdir($repodir);
-		foreach($cmd as $command) {
-			echo " *** " . $command . ":\n";
-			echo `$command`;
-		}
-        echo "INSTALLATION COMPLETE\n";
+        chdir(__DIR__ . "/..");
+        echo `git submodule update --init`;
     }
 
     public static function updateSubmodules($event)
     {
-		$repodir = __DIR__ . "/.."; 
-        $gitdir = $repodir . "/.git";
-
-		$cmd = array();
-        $cmd[] = "git status";
-        $cmd[] = "git submodule foreach pull";
-			
-		echo "START UPDATING\n";
-		chdir($repodir);
-		foreach($cmd as $command) {
-			echo " *** " . $command . ":\n";
-			echo `$command`;
-		}
-        echo "UPDATING COMPLETE\n";
+        chdir(__DIR__ . "/..");
+        echo `git submodule foreach git pull origin master`;
     }
 }
